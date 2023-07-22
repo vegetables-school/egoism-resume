ea0418e7067690ef075ddefc60b43f6ff93bdf78
Jimmy <jimmyrss1102@gmail.com>
2023-07-23 05:59:12 +0800

    feat: add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import { join } from "path";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), UnoCSS()],
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react(), UnoCSS()],
+    resolve: {
+      alias: {
+        '@': join(__dirname, "src"),
+      }
+    },
+  }
+
 })
